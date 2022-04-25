32b865b6e91118f4eb680b5b6c1e04a06ec4dbb2
Tiago Santos <trodrigues322@gmail.com>
2022-04-25 14:10:57 -0300

    feat(summary): adjust responsiveness

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   gap: 2rem;
   margin-top: -5rem;
 
